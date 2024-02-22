f222932315c26f2410033400eed1a6f695fa0d9b
552712 <552712@0338S-S-R0B6ZH0.sdcs.local>
2024-02-22 15:32:29 -0800

    Donkey and done

--- src/_99_extra/pin_the_tail/PinTheTail.java
+++ src/_99_extra/pin_the_tail/PinTheTail.java
@@ -14,6 +14,10 @@ public class PinTheTail extends PApplet {
     static final int HEIGHT = 800;
     PImage donkey;
     PImage tail;
+    int tailX;
+    int tailY;
+    boolean set = false;
+    int appeared = 0;
 
 
 
@@ -31,15 +35,49 @@ public class PinTheTail extends PApplet {
 
     @Override
     public void draw() {
-    	image(donkey, 0, 0, WIDTH, HEIGHT);
-    	image(tail, mouseX - 90, mouseY - 40);
+    	clear();
+    	if(dist(0,0, mouseX, mouseY) < 30) {
+    		image(donkey, 0, 0, WIDTH, HEIGHT);
+    		//System.out.println("Showing Donkey");
+    	} else {
+    		//System.out.println("Hiding donkey");
+    	}
+    	if(set){
+    		image(tail, tailX, tailY);
+    	} else {
+    		image(tail, mouseX - 90, mouseY - 40);
+    	}
     	
+    	if(mousePressed){
+    		for(int i = 0; i < mouseX - 90; i++) {
+    			tailX = i;
+    		}
+    		for(int i = 0; i < mouseY - 40; i++) {
+    			tailY = i;
+    		}
+    		set = true;
+        	//System.out.println("Set Tail");
+        }
+    	
+    	if(dist(105, 310, tailX, tailY) < 40) {
+    		playWhoohoo();
+    	}
+
     	rect(0, 0, 30, 30);
-    	rect(454, 75, 40, 40);
+    	rect(105, 310, 40, 40);
+    	
+    	if(appeared > 1000) {
+    		rect(0,0, 300, 300);
+    		textSize(24);
+    		text("Welcome to pin tail on the donkey, you will get to see where the donkey is when hover over the box in the corner"
+    				+ "\nHowever will be blind when you try to place the tail on the donkey",100,20);
+    		appeared++;
+    	}
     }
 
     static public void main(String[] args) {
         PApplet.main(PinTheTail.class.getName());
+        
     }
     
     /*********************** DO NOT MODIFY THE CODE BELOW ********************/
